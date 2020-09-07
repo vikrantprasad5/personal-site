c56b4706114f478289d51d8c5019cfe232135195
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-07 21:07:07 +0530

    Active with projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Vikrant Prasad', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: "Hello, I'm a",
   name: 'Vikrant Prasad',
-  subtitle: "I'm a full-stack web developer",
+  subtitle: "I'm an aspiring full-stack web developer",
   cta: '',
 };
 
@@ -29,30 +29,53 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'a.jpg',
+    title: 'Society Basket',
+    info:
+      'Developed an e-commerce website from which we can easily order grocery items in our locality, thus minimizing contact and bringing local shops to your door.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://society-basket.netlify.app',
+    repo: 'https://github.com/vikrantprasad5/society-basket', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'b.jpg',
+    title: 'Github User Stats',
+    info:
+      'Developed a web-application to fetch the stats of the any Github User by using the Github API and displaying their statistics using fusion charts.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://react-github-user-stats.netlify.app',
+    repo: 'https://github.com/vikrantprasad5/Github-Users', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'c.jpg',
+    title: 'Personal Blogging Site',
+    info:
+      'Made a beautiful minimalist personal blogging website which is fully functional with front-end and back-end capabilities where I can publish my blogs for everyone to read.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://github.com/vikrantprasad5/my-thoughts',
+    repo: 'https://my-thoughts26.herokuapp.com', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'd.jpg',
+    title: 'To-Do List',
+    info:
+      'Made a To-do List App and connected it with cloud database so that the list is stored and can be accessed from any network.',
+    info2: '',
+    url: 'https://github.com/vikrantprasad5/todo-list',
+    repo: 'https://morning-taiga-20548.herokuapp.com', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'e.jpg',
+    title: 'Simon Game',
+    info:
+      'Made an online version of popular electronic memorization game called Simon using javascript.',
+    info2: '',
+    url: 'https://github.com/vikrantprasad5/simon-game',
+    repo: 'https://vikrantprasad5.github.io/simon-game/', // if no repo, the button will not show up
   },
 ];
 
@@ -60,7 +83,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'vikrantprasad5@gmail.com',
 };
 
 // FOOTER DATA
@@ -69,27 +92,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/vikrantprasad26',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/vikrantprasad5/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/vikrantprasad5',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
